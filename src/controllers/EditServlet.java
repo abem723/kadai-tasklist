eeff48ab6788d041ee950d5651935ed6eabc41af
masatoshi.abe01 <masatoshi.abe01@g.softbank.co.jp>
2020-09-23 17:44:57 +0900

    adjust edit servlet and jsp for tasklist

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -13,9 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import models.Task;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class EditServlet
- */
+// editページのサーブレットクラス
 @WebServlet("/edit")
 public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -28,28 +26,28 @@ public class EditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+    // get処理メソッド
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         EntityManager em = DBUtil.createEntityManager();
 
-        // 該当のIDのメッセージ1件のみをデータベースから取得
-        Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+        // 該当のIDのタスク1件のみをデータベースから取得
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        // メッセージ情報とセッションIDをリクエストスコープに登録
-        request.setAttribute("message", m);
+        // タスク情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("task", t);
         request.setAttribute("_token", request.getSession().getId());
 
-        // メッセージデータが存在しているときのみ
-        // メッセージIDをセッションスコープに登録
-        if(m != null) {
-            request.getSession().setAttribute("message_id", m.getId());
+        // タスクデータが存在しているときのみ
+        // タスクIDをセッションスコープに登録
+        if(t != null) {
+            request.getSession().setAttribute("task_id", t.getId());
         }
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+        // editのビューを表示する
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
 }
